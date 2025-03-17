f2bb1404725e458408fcb69300920ee4494ed19a
Abdessamad Ait-bella <abdessamadaitbella1998@gmail.com>
2025-03-17 09:50:37 +0000

    ...

---
+++ app/Models/Offer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Offer extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected $fillable = [
+        'title',
+        'description',
+        'location',
+        'company_name',
+        'salary',
+        'job_type',
+        'deadline',
+        'is_active',
+        'user_id',
+    ];
+}
